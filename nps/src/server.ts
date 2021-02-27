7fee037fea13a3a9168c98e0cc5f58bee27947ed
Rayane <radharayg@gmail.com>
2021-02-27 18:16:42 -0300

    criando routes

--- nps/src/server.ts
+++ nps/src/server.ts
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res) => {
+    return res.json({message: 'Hello World' })
+})
+
+app.post('/', (req, res) => {
+    return res.json({message : 'Os dados foram salvos'})
+})
+
 app.listen(1313, () => console.log(`Server is running!`));
