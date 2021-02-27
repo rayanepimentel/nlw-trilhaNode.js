5222feb0ef61289bb07cc289c285ab2b1079c02e
Rayane <radharayg@gmail.com>
2021-02-27 20:56:24 -0300

    criando entities

--- nps/src/controllers/UsersController.ts
+++ nps/src/controllers/UsersController.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User'
 
 class UserController {
     async create(req: Request, res: Response) {
-        const body = req.body;
-        console.log(body);
-        return res.send();
+        const { name, email } = req.body;
+
+        const usersRespository = getRepository(User);
+        
+        const user = usersRespository.create({
+            name, email
+        })
+
+        await usersRespository.save(user);
+
+        return res.json(user);
     }
 }
 
